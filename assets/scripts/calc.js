6b3de42ed0091de4da1bd0a66b310fe7a305be3a
Alexsandrrh <sadovspbsasha@outlook.com>
2018-04-26 01:58:55 +0300

    update

--- assets/scripts/calc.js
+++ assets/scripts/calc.js
@@ -1,36 +1,36 @@
 $(document).ready(function () {
     //   ----------------------------
     $('.calc').change(function(){
-        var chbox1 = $('#check-1'),
+        let chbox1 = $('#check-1'),
             selectVal1 = $('select#select-1').val(),
             number1 = $('#number-1').val();
 
-        var chbox2 = $('#check-2'),
+        let chbox2 = $('#check-2'),
             selectVal2 = $('select#select-2').val(),
             number2 = $('#number-2').val();
 
-        var chbox3 = $('#check-3'),
+        let chbox3 = $('#check-3'),
             selectVal3 = $('select#select-3').val(),
             number3 = $('#number-3').val();
 
-        var chbox4 = $('#check-4'),
+        let chbox4 = $('#check-4'),
             selectVal4 = $('select#select-4').val(),
             number4 = $('#number-4').val();
 
-        var chbox5 = $('#check-5'),
+        let chbox5 = $('#check-5'),
             selectVal5 = $('select#select-5').val(),
             number5 = $('#number-5').val();
 
-        var chbox6 = $('#check-6'),
+        let chbox6 = $('#check-6'),
             selectVal6 = $('select#select-6').val(),
             number6 = $('#number-6').val();
 
-        var chbox7 = $('#check-7'),
+        let chbox7 = $('#check-7'),
             selectVal7 = $('select#select-7').val(),
             number7 = $('#number-7').val();
 
 
-        var sum1,
+        let sum1,
             sum2,
             sum3,
             sum4,
@@ -79,21 +79,28 @@ $(document).ready(function () {
         }else {
             sum7 = 0;
         }
-        var total = (sum1 + sum2 + sum3 + sum4 + sum5 + sum6 +sum7);
-        $('.result-cash').text(total.toFixed());
-        var percent;
-        if (total >= 500) {
+        let total = (sum1 + sum2 + sum3 + sum4 + sum5 + sum6 +sum7);
+        let percent;
+        if (total < 500) {
             percent = 95;
             total /= 100;
             total *= percent;
             $('.result-cash').text(total.toFixed());
             $('.result-discont').text(100 - percent);
-        }else if(total >= 1500) {
+        }else if (total < 1500) {
             percent = 90;
             total /= 100;
             total *= percent;
             $('.result-cash').text(total.toFixed());
             $('.result-discont').text(100 - percent);
+        }else if (total < 2500) {
+            percent = 85;
+            total /= 100;
+            total *= percent;
+            $('.result-cash').text(total.toFixed());
+            $('.result-discont').text(100 - percent);
+        }else {
+            $('.result-cash').text(total.toFixed());
         }
     });
 });
